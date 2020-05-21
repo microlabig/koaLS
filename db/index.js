d9d1dd144fd1fae6f10b795f1698dbbc99601a01
Igor Bezmestin <igor-rock@list.ru>
2020-05-21 21:10:11 +0300

    Подключил модели к проекту

--- db/index.js
+++ db/index.js
@@ -1,9 +1,32 @@
-const Sequelize = require('sequelize');
+const fs = require('fs');
 const path = require('path');
+const Sequelize = require('sequelize');
 const dbPort = process.env.DB_PORT || 5432;
 const env = process.env.NODE_ENV || 'development';
 const config = require(path.join(__dirname, 'config.js'))[env];
+const modelsFolderPath = './models';
+const models = {};
 
-const sequelize = new Sequelize(`postgres://${config.username}:${config.password}@${config.host}:${dbPort}/${config.database}`, config);
+const sequelize = new Sequelize(
+  `postgres://${config.username}:${config.password}@${config.host}:${dbPort}/${config.database}`,
+  config
+);
+
+fs
+  .readdirSync(path.join(__dirname, modelsFolderPath))
+  .forEach((file) => {
+    const model = sequelize['import'](
+      path.join(__dirname, modelsFolderPath, file)
+    );
+    models[model.name] = models;
+});
+
+Object.keys(models).forEach((modelName) => {
+  if (models[modelName].associate) {
+    models[modelName].associate(models);
+  }
+});
+
+sequelize.models = models;
 
 module.exports = sequelize;
